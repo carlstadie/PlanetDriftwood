fba94c45dad0f06af33d86811e3d57eb006f1a53
carlstadie <116877335+carlstadie@users.noreply.github.com>
2025-01-30 07:42:06 +0100

    Add files via upload

---
+++ main_2019.py
@@ -0,0 +1,33 @@
+# Main script to run preprocessing, training or prediction tasks.
+# Convert this to a notebook if you are using jupyter.
+
+# This is where you can change which config to use, by replacing 'config_default' with 'my_amazing_config' etc
+import config.config_2019 as configuration
+
+# INIT
+config = configuration.Configuration().validate()
+import preprocessing
+import training
+import prediction
+import postprocessing
+import evaluation
+
+if __name__ == "__main__":
+
+    # PREPROCESSING
+    # preprocessing.preprocess_all(config)
+
+    # TRAINING
+    # training.train_model(config)
+
+    # PREDICTION
+    # prediction.predict_all(config)
+
+    # POSTPROCESSING
+    # postprocessing.postprocess_all(config)
+
+    # EVALUATION
+    evaluation.evaluate_model(config)
+    
+    
+   
